90810964e1fd2567eba6521bf56681882004f9cc
JonathanHaws <jonathanmhaws1@gmail.com>
2023-04-14 17:23:13 -0600

    models

--- develop/database/models/User.js
+++ develop/database/models/User.js
@@ -1,14 +1,19 @@
 
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true, trim: true },
-  email: { type: String, required: true, unique: true, match: [/.+\@.+\..+/, 'Invalid email'] },
-  thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
-  friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-});
+const UserSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true, trim: true },
+    email: { type: String, required: true, unique: true, match: [/.+@.+\..+/, 'Invalid Email'] },
+    thoughts: [ { type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]},
+  {
+    toJSON: { virtuals: true, getters: true },
+    id: false
+  }
+);
 
-UserSchema.virtual('friendCount').get(function () {
+UserSchema.virtual('friendCount').get(function() {
   return this.friends.length;
 });
 
--- develop/database/seed.js
+++ develop/database/seed.js
@@ -3,24 +3,38 @@ const mongoose = require('mongoose');
 const db = require('./connection');
 const { User, Thought } = require('./models');
 
-const seedData = {
-  users: [
-    { username: "Jonathan", email: "jonathan@example.com" },
-    { username: "Mark", email: "mark@example.com" }
-  ],
-  thoughts: [
-    { thoughtText: "Automatic Test Seeded Thought 1", username: "Jonathan" },
-    { thoughtText: "Automatic Test Seeded Thought 2", username: "Mark" }
-  ]
-};
+const users = [
+  {
+    username: 'Jonathan',
+    email: 'jonathan@example.com',
+    thoughts: [
+      { thoughtText: 'Automatic Test Seeded Thought 1' }
+    ]
+  },
+  {
+    username: 'Mark',
+    email: 'mark@example.com',
+    thoughts: [
+      { thoughtText: 'Automatic Test Seeded Thought 2' }
+    ]
+  }
+];
 
 (async () => {
   try {
     await db;
     await User.deleteMany({});
     await Thought.deleteMany({});
-    await User.insertMany(seedData.users);
-    await Thought.insertMany(seedData.thoughts);
+    for (const user of users) {
+      const newUser = new User({ username: user.username, email: user.email });
+      const savedUser = await newUser.save();
+      for (const thought of user.thoughts) {
+        const newThought = new Thought({ ...thought, username: savedUser.username });
+        const savedThought = await newThought.save();
+        savedUser.thoughts.push(savedThought._id);
+      }
+      await savedUser.save();
+    }
     console.log('Seed successful');
     process.exit(0);
   } catch (error) {
